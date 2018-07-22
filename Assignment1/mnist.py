aae5738175f98f383f07062d1d026c69e8127603
Prateek Kumar <prateek@prateekkumar.in>
2018-07-22 15:33:36 +0530

    Add assignment 1
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

---
+++ Assignment1/mnist.py
@@ -0,0 +1,81 @@
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
+# Training settings
+batch_size = 64
+test_batch_size = 1000
+epochs = 10
+lr = 0.1
+
+seed = 1
+log_interval = 10
+
+torch.manual_seed(seed)
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=True, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=test_batch_size, shuffle=True)
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+
+model = Net()
+
+optimizer = optim.SGD(model.parameters(), lr=lr)
+
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = Variable(data), Variable(target)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data[0]))
+
+
+def test():
+    # TODO: Test the model on the test-set and report the loss and accuracy.
+    pass
+
+
+for epoch in range(1, epochs + 1):
+    train(epoch)
+    test()
--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Programming Assignments from CS5480 course during Jan - Apr 2018.
 
 ## Assignments
 
+1. Assignment 1
+
 ---
 Copyright (c) 2017 Prateek Kumar
